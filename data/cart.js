b8998ecf74a0f24c0665a179c2d48999deb31a83
chinmaykhiste2006-lgtm <chinmaykhiste2006@gmail.com>
2025-11-07 12:54:38 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+
+ cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
    quantity: 2
 }, {
@@ -6,6 +10,13 @@ export let cart = [{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1
 }];
+}
+
+
+function saveToStorage(){
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addtocart(productId,orderQuantity) {
 
@@ -26,6 +37,8 @@ if(matchingItem){
         quantity: orderQuantity
     });
 }
+
+saveToStorage();
 }
 
 export function total_items(){
@@ -56,4 +69,6 @@ export function removeFromCart(productId){
 
    cart = newCart;
 
+
+   saveToStorage();
 }
