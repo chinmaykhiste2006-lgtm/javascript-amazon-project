12e5ef1377435ef0002a617feb3b77d5f43989d2
chinmaykhiste2006-lgtm <chinmaykhiste2006@gmail.com>
2025-11-07 12:47:41 +0530

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -22,7 +22,7 @@ products.forEach((product) => {
 console.log(matchingProduct);
 
 cartsummaryHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -112,5 +112,8 @@ link.addEventListener('click', () => {
 
 const productId = link.dataset.productId;
 removeFromCart(productId);
+
+const container = document.querySelector(`.js-cart-item-container-${productId}`);
+container.remove();
 });
 });
