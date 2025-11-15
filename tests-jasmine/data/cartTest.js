f1f13bb4c034f35c888c96fa0f2283a9ccd0fc59
chinmaykhiste2006-lgtm <chinmaykhiste2006@gmail.com>
2025-11-15 13:17:00 +0530

    version1

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,21 @@
+import { addtocart, cart} from "../../data/cart.js";
+
+
+describe('test suite: addtocart', () => {
+
+    it('adds an existing product to the cart', () => {
+
+    });
+
+it('adds a new product to the cart', () => {
+spyOn(localStorage, 'getItem').and.callFake(() => {
+    return JSON.stringify([]);
+});
+console.log(localStorage.getItem('cart'));
+
+addtocart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+expect(cart.length).toEqual(1);
+});
+});
+
+
