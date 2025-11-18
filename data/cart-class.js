77bea2003fb6af97eb50c1231dba12c944ed8d49
Chinmay Rajesh Khiste <chinmaykhiste2006@gmail.com>
2025-11-18 17:08:38 +0530

    learn OOP

---
+++ data/cart-class.js
@@ -0,0 +1,131 @@
+//model
+class Cart{
+
+    cartItems = undefined;
+    localStorageKey = undefined;
+
+    constructor(localStorageKey) {
+
+this.localStorageKey = localStorageKey;
+this.loadFromStorage();
+    }
+
+    loadFromStorage(){
+       
+        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    
+    if(!this.cartItems){
+    
+     this.cartItems = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+       quantity: 2,
+       deliveryOptionId: '1'
+    }, {
+    
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: '2'
+    }];
+    }
+    
+    }
+
+    saveToStorage(){
+       
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    }
+
+
+    addtocart(productId,orderQuantity) {
+       
+        let matchingItem;
+    
+    this.cartItems.forEach((item) => {
+    if(productId === item.productId){
+    matchingItem = item;
+    }
+    });
+    
+    if(matchingItem){
+    
+        matchingItem.quantity += orderQuantity;
+    } else{
+      this.cartItems.push({
+            productId: productId,
+            quantity: orderQuantity,
+            deliveryOptionId: '1'
+        });
+    }
+    
+    this.saveToStorage();
+    }
+
+    total_items(){
+       
+        let cartQuantity = 0;
+    
+    this.cartItems.forEach((item) => {
+    
+        cartQuantity += item.quantity;
+    });
+    
+    return cartQuantity;
+    }
+
+    removeFromCart(productId){
+       
+        const len = this.cartItems.length;
+          for(let i=0;i<len;i++){
+        
+            if(this.cartItems[i].productId===productId){
+        
+                this.cartItems.splice(i,1);
+                break;
+            }
+          }
+        
+        
+           this.saveToStorage();
+        }
+
+        updateDeliveryOption(productId,deliveryOptionId){
+       
+            let matchingItem;
+        
+        this.cartItems.forEach((item) => {
+        if(productId === item.productId){
+        matchingItem = item;
+        }
+        });
+        
+        
+        matchingItem.deliveryOptionId =deliveryOptionId;
+        this.saveToStorage();
+    }
+
+}
+
+const cart = new Cart('cart-oop');
+
+
+const businessCart = new Cart('business-oop');
+
+
+ 
+
+
+
+console.log(cart);
+console.log(businessCart);
+
+console.log(businessCart instanceof Cart);
+
+
+
+
+
+
+
+
+
+
