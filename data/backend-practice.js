7adc4b841ae083a7634b8660a7eeb834b387c772
Chinmay Rajesh Khiste <chinmaykhiste2006@gmail.com>
2025-11-24 19:42:40 +0530

    delete app.jsx

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -2,9 +2,8 @@ const xhr = new XMLHttpRequest();
 
 
 xhr.addEventListener('load', () => {
-console.log(xhr.response());
+console.log(xhr.response);
 });
 
-
-xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.open('GET', 'https://supersimplebackend.dev/hello');
 xhr.send();
